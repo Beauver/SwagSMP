6820707899120dab832b0ab54fb55584eea779ab
Beauver <bvvergaming@gmail.com>
2023-08-11 15:48:39 +0200

    Basics

---
+++ SwagSMP/src/main/java/com/beauver/swagsmp/SwagSMPCore.java
@@ -0,0 +1,17 @@
+package com.beauver.swagsmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SwagSMPCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
